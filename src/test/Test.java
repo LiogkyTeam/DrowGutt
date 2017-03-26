feb894b95d4e83f1263a07eae75b38b3a13e9e72
Alexey <al.liogky@yandex.ru>
2017-03-27 02:29:04 +0300

    Add Test class for temp quick experiments

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Alexey on 27.03.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This test class for testing different features ");
+    }
+}
