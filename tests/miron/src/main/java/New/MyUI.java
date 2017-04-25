1c6e3aad3d6284179118c0039832cb3220bcde92
MironKuznetsov <uthjq1@mail.ru>
2017-04-25 08:24:24 +0300

    Test: Notifications@

--- tests/miron/src/main/java/New/MyUI.java
+++ tests/miron/src/main/java/New/MyUI.java
@@ -4,11 +4,18 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.Page;
-import com.vaadin.server.AbstractClientConnector;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
+import com.vaadin.event.MouseEvents;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.*;
+import com.vaadin.shared.Position;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.MenuBar;
+import sun.font.FontAccess;
+import com.vaadin.ui.Button.ClickEvent;
+
+import java.awt.*;
+import java.awt.Button;
 import java.lang.Object;
 
 
@@ -30,6 +37,8 @@ public class MyUI extends UI {
         final TopUiInTheWorld tuintw = new TopUiInTheWorld();
         final Label label = new Label("This Needs to be Colored");
         label.addStyleName("coloredlabel");
+        final com.vaadin.ui.Button pencilButton = new com.vaadin.ui.Button();
+        final com.vaadin.ui.Button rectangleButton = new com.vaadin.ui.Button();
 
 // Some UI logic to change CSS
         final ColorPicker sample = new ColorPicker("Set the Color Here");
@@ -41,19 +50,41 @@ public class MyUI extends UI {
 
         sample.addValueChangeListener(event -> Notification.show(
                 "Color changed: " + event.getValue().getCSS()
-                ));
+        ));
 
-        tuintw.pencilButton.addClickListener( e -> {
+
+
+        final com.vaadin.ui.Button button = new com.vaadin.ui.Button("Do not press this button");
+
+        button.addClickListener( e-> {
+             {
+                // Notification.show("Do not press this button again");
+            }
+        });
+
+
+
+        pencilButton.addClickListener( e -> {
 
 
             layout.addComponents(label, sample);
             layout1.addComponent(layout);
         });
-        layout1.addComponents(tuintw);
+
+        layout1.addComponents(pencilButton);
         setContent(layout1);
 
 
 
+       /* final HorizontalLayout layout1 = new HorizontalLayout();
+        final MenuBar menu = new MenuBar();
+        final Label label = new Label("This Needs to be Colored");
+        final MenuBar.MenuItem pencil = menu.addItem("Pencil", FontAwesome.PENCIL, null);
+        final MenuBar.MenuItem size = pencil.addItem("Size",null,null);
+        size.addItem("1", null);//new ThemeResource("1"), null);
+        layout1.addComponents(menu, label);
+        setContent(layout1);
+*/
 
     }
 
