4e95240f75b1da47361168abb3b225302a0c40d2
egorgladin <egorgladin@yandex.ru>
2017-05-22 14:18:05 +0300

    Create LoginPage.java
    User types ligin and password in the login page

---
+++ application/src/main/java/org/test/ui/LoginPage.java
@@ -0,0 +1,51 @@
+package org.test.ui;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.*;
+
+public class LoginPage extends VerticalLayout implements View {
+	public static final String NAME = "";
+
+	public LoginPage(){
+		// add login panel to the layout
+		Panel panel = new Panel("Login");
+		panel.setSizeUndefined();
+		addComponent(panel);
+		
+		// add layout with username and password fields
+		FormLayout content = new FormLayout(); /* FormLayout lays the components and their captions out in two columns */
+		TextField username = new TextField("Username");
+		content.addComponent(username);
+		PasswordField password = new PasswordField("Password");
+		content.addComponent(password);
+		
+		// add button for sending credentials
+		Button send = new Button("Enter");
+		send.addClickListener(new ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if(VaadinloginUI.AUTH.authenticate(username.getValue(), password.getValue())) {
+					VaadinSession.getCurrent().setAttribute("user", username.getValue());
+					getUI().getNavigator().addView(SecurePage.NAME, SecurePage.class);
+					getUI().getNavigator().addView(OtherSecurePage.NAME, OtherSecurePage.class);
+					Page.getCurrent().setUriFragment("!"+SecurePage.NAME);
+				} else {
+					Notification.show("Invalid credentials", Notification.Type.ERROR_MESSAGE);
+				}
+			}
+		});
+		content.addComponent(send);
+		content.setSizeUndefined();
+		content.setMargin(true);
+		panel.setContent(content);
+		setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
+	}
+	
+	@Override
+	public void enter(ViewChangeEvent event) {
+	}
+
+}
