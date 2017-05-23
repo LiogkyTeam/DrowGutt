50a24c7df7d27c3c0604f061b50ab7988f79a264
Fenfiriy <stephan.petrovvv@gmail.com>
2017-05-23 08:11:53 +0300

    Drawers

--- canv-addon/src/main/java/org/test/client/item/objects/Curve.java
+++ canv-addon/src/main/java/org/test/client/item/objects/Curve.java
@@ -1,24 +1,27 @@
 package org.test.client.item.objects;
 
+import com.google.gwt.canvas.dom.client.Context2d;
 import org.test.client.CanvasWidget;
 import org.test.client.item.Item;
+
+import java.util.ArrayList;
 
 /**
  * Created by Alexey on 16.05.2017.
  */
 public class Curve extends Item {
-    private Point[] curve;
+    private ArrayList<Point> curve;
 
     public Curve() {
     }
 
-    public Curve(Point[] curve) {
+    public Curve(ArrayList<Point> curve) {
         this.curve = curve;
-        setBorders(curve[0].getX(), curve[0].getX(), 0, 0);
+        setBorders(curve.get(0).getX(), curve.get(0).getX(), 0, 0);
        setBorders(curve);
     }
 
-    private void setBorders(Point[] points){
+    private void setBorders(ArrayList<Point> points){
         int x_min = getX_min(), x_max = getX_max(),
                 y_min = getY_min(), y_max = getY_max();
         for (Point point: points) {
@@ -36,11 +39,16 @@ public class Curve extends Item {
 
     public void draw(CanvasWidget canvas)
     {
-        curve[0].draw(canvas);
-        for (int i = 1; i < curve.length; i++) {
-            canvas.moveTo((double) curve[i - 1].getX(), (double) curve[i - 1].getY());
-            curve[i].draw(canvas);
-            canvas.lineTo((double) curve[i].getX(), (double) curve[i].getY());
+        curve.get(0).draw(canvas);
+        canvas.moveTo((double) curve.get(0).getX(), (double) curve.get(0).getY());
+        Context2d ctx = canvas.getContext2d();
+        ctx.beginPath();
+        ctx.setLineWidth(5);
+        ctx.setStrokeStyle(canvas.color);
+        for (int i = 1; i < curve.size(); i++) {
+            curve.get(i).draw(canvas);
+            canvas.moveTo((double) curve.get(i).getX(), (double) curve.get(i).getY());
+            canvas.lineTo((double) curve.get(i).getX(), (double) curve.get(i).getY());
         }
     }
 }
